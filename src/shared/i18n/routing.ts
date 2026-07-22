a1a73b3d6fb1032fa11b0e7260ec86b172c9ffc0
Ju Sang Han <hjs000528@gmail.com>
2026-07-22 17:52:00 +0900

    ninth commit

--- src/shared/i18n/routing.ts
+++ src/shared/i18n/routing.ts
@@ -3,6 +3,7 @@ import { defineRouting } from 'next-intl/routing'
 export const routing = defineRouting({
   locales: ['ko', 'en'],
   defaultLocale: 'ko',
+  localeDetection: false,
 })
 
 export type Locale = (typeof routing.locales)[number]
